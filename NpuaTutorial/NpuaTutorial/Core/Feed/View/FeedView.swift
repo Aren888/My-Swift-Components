b09a43b6750a09e6c9e85d2f021ffb669ba57961
Aren <aren.musayelyan555@gmail.com>
2023-12-01 22:39:53 +0400

    Create initial UI for FeedView

--- NpuaTutorial/NpuaTutorial/Core/Feed/View/FeedView.swift
+++ NpuaTutorial/NpuaTutorial/Core/Feed/View/FeedView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Hello, Feed")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { thread in
+                        ThreadCell()
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG: Refresh Threads")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    // Add your button action here
+                } label: {
+                    Image(systemName: "arrow.counterclockwise")
+                        .foregroundStyle(.black)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    FeedView()
+    NavigationStack {
+        FeedView()
+    }
 }
