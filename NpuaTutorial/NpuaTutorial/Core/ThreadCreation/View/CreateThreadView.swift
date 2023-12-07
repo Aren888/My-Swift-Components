0403524be1ace1c7c985c81d9eb45b72ecda5a2b
Aren888 <aren.musayelyan555@gmail.com>
2023-12-08 01:14:57 +0400

    Added Create Thread Section

--- NpuaTutorial/NpuaTutorial/Core/ThreadCreation/View/CreateThreadView.swift
+++ NpuaTutorial/NpuaTutorial/Core/ThreadCreation/View/CreateThreadView.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    
+    @State private var caption = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text("Hello, Create Thread")
+        NavigationStack {
+            VStack {
+                HStack {
+                    CircularProfileImageView()
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("maxverstappen1")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Start a Thread...", text: $caption, axis: .vertical)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if !caption.isEmpty {
+                        Button {
+                            caption = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    
+                }
+                Spacer()
+                
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .font(.footnote)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .opacity(caption.isEmpty ? 0.5 : 1)
+                    .disabled(caption.isEmpty)
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
+#if DEBUG
 #Preview {
     CreateThreadView()
 }
+#endif
