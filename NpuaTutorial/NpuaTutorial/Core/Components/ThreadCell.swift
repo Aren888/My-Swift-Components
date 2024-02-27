22d9836d8d728e3798395092160fcc797f4ab093
Aren888 <aren.musayelyan555@gmail.com>
2024-02-27 22:42:24 +0400

    Add True Spacing

--- NpuaTutorial/NpuaTutorial/Core/Components/ThreadCell.swift
+++ NpuaTutorial/NpuaTutorial/Core/Components/ThreadCell.swift
@@ -15,34 +15,23 @@ struct ThreadCell: View {
         VStack(alignment: .center) {
             
             HStack(alignment: .center) {
-                
-                CircularProfileImageView(user: thread.user, size: .small)
+                VStack() {
+                    CircularProfileImageView(user: thread.user, size: .small)
+                    
+                    Spacer(minLength: 0)
+                }
                 
                 VStack(alignment: .leading, spacing: 6) {
                     HStack(spacing: 0) {
-                        
-                        if let userName = thread.user?.userName {
-                            Text(userName)
-                                .font(.footnote)
-                                .fontWeight(.semibold)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .frame(height: 14)
-                            
-                        } else {
-                            LoadingView()
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .frame(width: 60, height: 14)
-                                .padding(.trailing, 80)
-                            
-                                .cornerRadius(9)
-                            
-                        }
+                        Text(thread.user?.userName ?? "")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
                         
                         Text(thread.timestamp.timestampString())
                             .font(.caption)
                             .foregroundStyle(Color(.systemGray3))
                             .frame(maxWidth: .infinity, alignment: .trailing)
-                            .frame(height: 14)
                             .padding(.trailing, 10)
                         
                         Button(action: {
@@ -52,23 +41,15 @@ struct ThreadCell: View {
                                 .foregroundStyle(Color(.darkGray))
                         }
                     }
-                    if (thread.user?.userName) != nil {
-                        Text(thread.caption)
-                            .font(.footnote)
-                            .multilineTextAlignment(.leading)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .frame(height: 14)
-                    } else {
-                        LoadingView()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .frame(width: 80, height: 14)
-                            .cornerRadius(9)
-                    }
+                    Text(thread.caption)
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
                     HStack(spacing: 16) {
-                        Button(action: {
-                            
-                        }) {
+                        Button(action: {}) {
                             Image(systemName: "heart")
+                                .foregroundColor(.black)
                         }
                         
                         Button(action: {
@@ -90,11 +71,13 @@ struct ThreadCell: View {
                         }
                     }
                     .foregroundStyle(.black)
+                    
+                    Spacer(minLength: 0)
                 }
             }
-            .padding()
-            .frame(height: 70)
-
+            .padding(.vertical, 6)
+            .padding(.horizontal)
+            
             Divider()
         }
     }
