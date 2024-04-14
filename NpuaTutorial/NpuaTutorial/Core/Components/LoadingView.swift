3999f1e3f3ffd6b22c484dccdee7cae68f5b1365
Aren888 <aren.musayelyan555@gmail.com>
2024-04-14 12:48:22 +0400

    Refactor ExploreView

---
+++ NpuaTutorial/NpuaTutorial/Core/Components/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  NpuaTutorial
+//
+//  Created by Solicy Ios on 14.04.24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
